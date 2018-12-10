17a8995c661db7ac3f91b26b8222e9229eb08165
Adam Bodie <adam.bodie@yahoo.com>
2018-12-09 16:09:09 -0800

    Added header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import image from '../header.jpg';
 
 const Header = () => (
 				<header>
@@ -7,6 +7,7 @@ const Header = () => (
 						<div className="top-bar-left">
 							<h4>Adam's Birthright Trip to The Land of Israel</h4>
 						</div>
+						<img src={image} alt="RT-38-1047" />
 					</div>
 				</header>
 );
