a729560a01f9a28a0f2a2a1790e9c318b9e5b7a8
Adam Bodie <adam.bodie@yahoo.com>
2018-12-10 18:51:46 -0800

    Added new tests

--- src/tests/Photo.test.js
+++ src/tests/Photo.test.js
@@ -2,21 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import { shallow, mount, render } from 'enzyme';
-import { MemoryRouter } from 'react-router';
-import Landing from '../components/Landing';
+import Photo from '../components/Posts/Photo';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	ReactDOM.render(<MemoryRouter><Landing /></MemoryRouter>, div);
+	ReactDOM.render(<Photo />, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
 
 it('renders correctly', () => {
-	const tree = renderer.create(
-    <MemoryRouter>
-      <Landing />
-    </MemoryRouter>
-  ).toJSON();
+	const tree = renderer.create(<Photo id="1" title="title" index="2" length="10"/>).toJSON();
 	expect(tree).toMatchSnapshot();
 });
 
+it('renders a `photo`', () => {
+	const wrapper = shallow(<Photo />);
+    expect(wrapper.find('img').length).toEqual(1);
+ });
--- src/tests/Post.test.js
+++ src/tests/Post.test.js
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import { shallow, mount, render } from 'enzyme';
-import { MemoryRouter } from 'react-router';
-import Landing from '../components/Landing';
+import Post from '../components/Posts/Post';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	ReactDOM.render(<MemoryRouter><Landing /></MemoryRouter>, div);
+	ReactDOM.render(<Post />, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
 
 it('renders correctly', () => {
-	const tree = renderer.create(
-    <MemoryRouter>
-      <Landing />
-    </MemoryRouter>
-  ).toJSON();
+const tree = renderer.create(<Post title="Title" paragraph={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."} />).toJSON();
 	expect(tree).toMatchSnapshot();
 });
+
+it('renders a `title`', () => {
+	const wrapper = shallow(<Post title="Title"/>);
+    expect(wrapper.find('h1').length).toEqual(1);
+ });
 
