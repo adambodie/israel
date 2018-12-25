27785793a9cf7ef002a444137279da292e21f3de
adambodie <adam.m.bodie@gmail.com>
2018-12-24 18:38:25 -0800

    Added home button from Font Awesome on Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import FontAwesome from 'react-fontawesome';
 
 const Header = () => (
 				<header>
 					<div className="top-bar">
 						<div className="top-bar-left">
 							<h4>Adam's Birthright Trip to The Land of Israel</h4>
+							<Link to={'/index'}>
+								<FontAwesome
+									name='home'
+									style={{ textShadow: '2px 2px 4px black' }}
+									/>
+							</Link>
 						</div>
 						<img src={'https://s3-us-west-2.amazonaws.com/birthright-israel.bodiewebdesign.com/images/main/header.jpg'} alt="RT-38-1047" />
 					</div>
