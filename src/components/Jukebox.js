6f462e670387f71fa241a5598c8e40075467833b
adambodie <adam.m.bodie@gmail.com>
2018-12-29 12:02:32 -0800

    updated Jukebox component with React-Scrollbar dependency

--- src/components/Jukebox.js
+++ src/components/Jukebox.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Column } from 'react-foundation';
+import ScrollArea from 'react-scrollbar';
 
 const songs = [
 	{title: 'Tel Aviv', src: 'nMQw29nfzpg'},
@@ -12,17 +12,23 @@ const songs = [
 ]
 const Jukebox = () => {
   return (
-		<Row className="display grid-x big-jukebox">
-			<Column medium={6} offsetOnMedium={3}>
-			<div className="jukebox">
+		<div className="jukebox">
+			<div className="jukebox-title">
 				<h1>Sababa!</h1>
 			</div>
-			</Column>
-			<Column medium={6} offsetOnMedium={3} className="playlist">
+			<ScrollArea
+				speed={0.8}
+				className="area"
+				contentClassName="content"
+				horizontal={false}
+				>
 				{songs.map((x, index) => <iframe title={x.title} key={index}  src={`https://www.youtube.com/embed/${x.src}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>)}
-			</Column>
-		</Row>
-
+			</ScrollArea>
+			<div className="legs">
+				<div className="leg"></div>
+				<div className="leg"></div>
+			</div>
+		</div>
   );
 };
 
