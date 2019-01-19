3f4351262c95afd75d4a36ba5dba6bd4d7fd560c
Adam Bodie <adam.bodie@yahoo.com>
2019-01-19 15:32:26 -0800

    Added new logo for scrollspy component

--- src/components/Main/MainScrollspy.js
+++ src/components/Main/MainScrollspy.js
@@ -8,7 +8,7 @@ const MainScrollspy = () => {
 		<Consumer>
 			{ ({ pages }) => (
 				<div className="o-sidebar o-sidebar--fixed-left c-side-nav">
-				<div className="logo"></div>
+				<img className="logo" src='https://s3-us-west-2.amazonaws.com/birthright-israel.bodiewebdesign.com/images/main/logo.gif' alt="logo" />
 					<nav className="c-side-nav__body">
 						<Scrollspy items={ pages.map((x)=> x.scroll) } currentClassName="is-current" className="c-side-nav__list nav-list" offset={-20}>
 							{pages.map((x)=> { 
@@ -23,7 +23,7 @@ const MainScrollspy = () => {
 											{x.items.map((y, index) => {
 												return(
 												<div style={{fontSize: '12px'}} key={index}>
-													<a href={`#${y.link}`}  className="c-side-nav__sublink">{y.linkTitle}</a>
+													<a href={`#${y.link}`}  className="c-side-nav__link">{y.linkTitle}</a>
 												</div>
 												)}
 											)}
