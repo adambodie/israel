81bbdb4e8d43694e2103cdbdf3866fea4dee129e
Adam Bodie <adam.bodie@yahoo.com>
2019-01-29 18:24:49 -0800

    Added more mobile styles

--- src/components/Main/MainScrollspy.js
+++ src/components/Main/MainScrollspy.js
@@ -7,16 +7,36 @@ import MainScrollspyItem from '../../components/Main/MainScrollspyItem.js';
 const MainScrollspy = () => (
 		<Consumer>
 			{ ({ pages }) => {
+					const getMobileOperatingSystem = () => {
+						const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+						  // Windows Phone must come first because its UA also contains "Android"
+						  if (/windows phone/i.test(userAgent)) {
+							  return "Windows Phone";
+						  }
+						  if (/android/i.test(userAgent)) {
+							  return "Android";
+						  }	  
+						  // iOS detection 
+						  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+							  return "iOS";
+						  }	  
+						  return "unknown";
+					  }
+				const isMobile = getMobileOperatingSystem() === 'unknown' ? '#' : '';
+				let className = 'mobile-collapse';
+				if (getMobileOperatingSystem() !== 'unknown' && window.width < 768) {
+					className = className + 'd'
+				}
 				return (
 				<div className='o-sidebar o-sidebar--fixed-left c-side-nav'>
 					<img className='logo' src={`${mainLink}/logo.gif`} alt='logo' />
-					<div className='mobile-collapse'>
-					<Collapsible trigger=''>
-						<MainScrollspyItem startIndex={0} scrollLink={''} />
-					</Collapsible>
+					<div className={className}>
+						<Collapsible trigger=''>
+							<MainScrollspyItem startIndex={0} scrollLink={isMobile} />
+						</Collapsible>
 					</div>
 					<nav className='c-side-nav__body'>
-						<MainScrollspyItem startIndex={100} scrollLink={'#'}/>
+						<MainScrollspyItem startIndex={100} scrollLink={isMobile}/>
 					</nav>
 				</div>
 			)}
