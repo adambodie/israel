9e65229015f92499f7e3dace7a074487bebca40b
unknown <adam.m.bodie@gmail.com>
2019-12-15 11:46:45 -0800

    Fixed broken links

--- src/components/Shakshuka/ShakshukaImage.js
+++ src/components/Shakshuka/ShakshukaImage.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ShakshukaImage = ({title, image}) => (
 	<React.Fragment>
 		<h2>{title}</h2>
-		<img className='thumbnail' src={`/assets/IMG_45${image}.JPG`} alt={title}/>
+		<img className='thumbnail' src={`/assets/photos/IMG_45${image}.JPG`} alt={title}/>
 	</React.Fragment>
 )
 
